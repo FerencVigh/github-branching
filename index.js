fe8a3781a15f3f9402115a8e88100a7bbc1e0cd1
Ferenc Vigh <fvigh.dev@gmail.com>
2024-02-11 17:52:20 +0100

    menu button functionality to toggle menu added

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+let menuButton = document.querySelector(".header__burger-button");
 
+menuButton.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  menuButton.classList.toggle("header__burger-button--active");
+}
